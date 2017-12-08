48e0f165d4c32d78c0a8dcf3d635258d8a5909e6
ccadenc <ccadenc@amazon.com>
2017-12-08 08:55:54 -0700

    lookify darnit

---
+++ springProjects/Lookify/src/main/java/com/cadence/lookify/repositories/SongRepo.java
@@ -0,0 +1,22 @@
+package com.cadence.lookify.repositories;
+
+import java.util.List;
+
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.codingdojo.lookify.models.Song;
+
+public class SongRepo {
+
+	
+	@Repository
+	public interface SongRepo extends CrudRepository<Song, Long>{
+		List<Song> findAll();
+		List<Song> findTop10ByOrderByRatingDesc();
+		List<Song> findByArtistContaining(String query);
+	}
+	
+}
---
+++ springProjects/Lookify/src/main/java/com/cadence/services/SongService.java
@@ -0,0 +1,42 @@
+package com.cadence.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.codingdojo.lookify.models.Song;
+import com.codingdojo.lookify.repositories.SongRepo;
+@Service
+public class SongService {
+	private final SongRepo songRepo;
+	
+	public SongService(SongRepo songRepo) {
+		this.songRepo = songRepo;
+	}
+	
+	
+	public List<Song> findAll() {
+		return songRepo.findAll();
+	}
+	
+	public void addSong(Song song) {
+		songRepo.save(song);
+	}
+	public Song findOne(Long id) {
+		return songRepo.findOne(id);
+	}
+	
+	public void deleteSong(Long id) {
+		songRepo.delete(id);
+	}	
+	public List<Song> searchByArtist(String artist) {
+		return songRepo.findByArtistContaining(artist);
+	}
+	public List<Song> searchTopTen() {
+		return songRepo.findTop10ByOrderByRatingDesc();
+	}
+	
+	
+}
+
+	
